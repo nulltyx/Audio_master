892360085282c02c29a7e5c58d4c37a358ab0c41
nulltyx <68776923+nulltyx@users.noreply.github.com>
2026-02-13 20:48:49 +0000

    added some style, made a mute button

--- main.py
+++ main.py
@@ -12,8 +12,6 @@ def set_volume(v1):
         subprocess.run(["amixer", "sset", "Master", f"{int(v1)}%"])  #sets the volume
     if shutil.which("wpctl"):     #fedora audio system
         subprocess.run(["wpctl", "set-volume", "@DEFAULT_AUDIO_SINK@", f"{int(v1)}%"])  
-    if shutil.which("pactl"):
-        subprocess.run(["pactl", "set-sink-volume", "@DEFAULT_SINK@", f"{int(v1)}%"])  
     
 
 def get_volume():   #finds what vol im at
@@ -25,19 +23,25 @@ def get_volume():   #finds what vol im at
         result = subprocess.run(["wpctl", "set-volume", "@DEFAULT_AUDIO_SINK@"],capture_output=True,text=True)
         match = re.search(r"\[(\d+)%\]", result.stdout)   
         return int(match.group(1))      
-    if shutil.which("pactl"):
-        result = subprocess.run(["pactl", "set-sink-volume", "@DEFAULT_SINK@"],capture_output=True,text=True)
-        match = re.search(r"\[(\d+)%\]", result.stdout)   
-        return int(match.group(1))     
     
+
+def mute():
+    if shutil.which("amixer"):   #ubuntu audio system
+        subprocess.run(["amixer", "sset", "Master", "0%"])  #sets the volume
+    if shutil.which("wpctl"):     #fedora audio system
+        subprocess.run(["wpctl", "set-volume", "@DEFAULT_AUDIO_SINK@", "0%"])  
 
 
 win = Tk()    #start of window
 win.geometry("400x200")
 
+
+
 v1 = DoubleVar()
 
-Label(win, text="Volume:").pack()
+l=Label(win, text="Volume:",)
+l.pack()
+
 
 
 vol = Scale(   #slider
@@ -51,8 +55,16 @@ vol = Scale(   #slider
 vol.pack()
 
 
+Button(win, text="Mute", command=mute).pack()
+Button(win, text="Unmute", command= unmute).pack()  #doesnt do anything yet
 
-
+f = subprocess.check_output(["gsettings", "get", "org.gnome.desktop.interface", "color-scheme"],text=True).strip()  #checks if im usind dark or light mode
+if f == "'prefer-dark'":
+    win.config(background="#636363") #sets dark mode on all the things
+    vol.config(background="#636363")
+    l.config(background="#636363")
+else:
+    pass
 
 v1.set(get_volume()) #sets the volume to the current volume
 
